138fa5c84c9999500a0de9734fb054b322c857cc
mor3162 <more3031996@gmail.com>
2024-09-12 10:23:00 +0300

    forth commit

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require("express");
-
+const cors = require("cors");
 const connectToDb = require("./DB/dbService");
 const router = require("./router/router");
 const app = express();
 
 const PORT = 8181;
 
+// middleware to handle CORS
+app.use(cors());
+
 // middleware to parse incoming request
 app.use(express.json());
 
 // middleware to route the request - בודק לאיזה נתיב הבקשה מתאימה
 app.use(router);
+
+
 
 app.listen(PORT, () => {
     console.log("app is listening to port " + PORT);
