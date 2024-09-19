5c4d0e06db519ff5f9a1b9e2da210508090a7305
mor3162 <more3031996@gmail.com>
2024-09-19 15:50:45 +0300

    cross

--- DB/mongodb/connectToAtlas.js
+++ DB/mongodb/connectToAtlas.js
@@ -5,7 +5,7 @@ const CONNECTION = "";
 
 const connectToAtlasDb = async () => {
     try {
-        await mongoose.connect(CONNECTION);
+        // await mongoose.connect(CONNECTION);
         console.log('Successfully connected to MongoDB in Atlas');
     } catch (error) {
         console.error('Could not connect to MongoDB', error);
